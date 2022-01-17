99c320f606b56700a7508f1020058ac07a216a4f
lel <88410471+Iamlel@users.noreply.github.com>
2022-01-16 18:22:39 -0800

    Update 976.c

--- 976.c
+++ 976.c
@@ -1,5 +1,10 @@
+int cmpFunc(const int* a, const int* b)
+{
+    return (*b - *a);
+}
+
 int largestPerimeter(int* nums, int numsSize) {
-    qsort(nums, numsSize); // sort in descending order
+    qsort(nums, numsSize, sizeof(int), cmpFunc);
     int large, small;
     for (int i = 0; i < numsSize - 2; i++) {
         large = nums[i], small = (nums[i + 1] + nums[i + 2]);
@@ -7,3 +12,17 @@ int largestPerimeter(int* nums, int numsSize) {
     }
     return 0;
 }
+
+/*
+Faster
+
+int largestPerimeter(int* nums, int numsSize) {
+    qsort(nums, numsSize, sizeof(int), cmpFunc);
+    for (int i = 0; i < numsSize - 2; i++) {
+        if ((nums[i + 1] + nums[i + 2]) > nums[i]) { return (nums[i + 1] + nums[i + 2] + nums[i]); }
+    }
+    return 0;
+}
+
+
+*/

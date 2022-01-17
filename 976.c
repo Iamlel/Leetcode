0816a127f93f61b73ee644c363925c8e0488fe9e
lel <88410471+Iamlel@users.noreply.github.com>
2022-01-16 18:11:08 -0800

    Create 976.c

---
+++ 976.c
@@ -0,0 +1,9 @@
+int largestPerimeter(int* nums, int numsSize) {
+    sort(nums, numsSize); // sort in descending order
+    int large, small;
+    for (int i = 0; i < numsSize - 2; i++) {
+        large = nums[i], small = (nums[i + 1] + nums[i + 2]);
+        if (small > large) { return (small + large); }
+    }
+    return 0;
+}
